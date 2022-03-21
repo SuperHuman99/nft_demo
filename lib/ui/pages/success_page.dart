16a54916a08a8d72a39f8c646db2c76602d21ad5
SuperHuman99 <lilkhendrick1040@gmail.com>
2022-03-21 17:19:04 +0000

    "Fixed back press navigating back from scale animation to success_page.dart"

--- lib/ui/pages/success_page.dart
+++ lib/ui/pages/success_page.dart
@@ -16,74 +16,79 @@ class _SuccessPageState extends State<SuccessPage> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: secondaryColor,
-        body: Container(
-          height: screenHeight,
-          width: screenWidth,
-          padding: EdgeInsets.symmetric(horizontal: 0.05 * screenWidth),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Spacer(
-                flex: 5,
-              ),
-              const FadeInAnimation(
-                delay: 300,
-                child: Text(
-                  "Great !",
-                  style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+    return WillPopScope(
+      onWillPop: () async{
+        return false;
+      },
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: secondaryColor,
+          body: Container(
+            height: screenHeight,
+            width: screenWidth,
+            padding: EdgeInsets.symmetric(horizontal: 0.05 * screenWidth),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Spacer(
+                  flex: 5,
+                ),
+                const FadeInAnimation(
+                  delay: 300,
+                  child: Text(
+                    "Great !",
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const FadeInAnimation(
-                delay: 800,
-                child: Text(
-                  "We send an email,",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                const FadeInAnimation(
+                  delay: 800,
+                  child: Text(
+                    "We send an email,",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const FadeInAnimation(
-                delay: 1200,
-                child: Text(
-                  successText,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
+                const FadeInAnimation(
+                  delay: 1200,
+                  child: Text(
+                    successText,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const Spacer(
-                flex: 5,
-              ),
-              FadeInAnimation(
-                delay: 1600,
-                child: CustomElevatedButton(
-                  buttonText: "Back Home",
-                  onPressed: () {
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                        builder: (context) => const LandingPage(),
-                      ),
-                      (route) => true,
-                    );
-                  },
-                  width: 0.9 * screenWidth,
-                  color: primaryColor,
+                const Spacer(
+                  flex: 5,
                 ),
-              ),
-              const Spacer(
-                flex: 1,
-              ),
-            ],
+                FadeInAnimation(
+                  delay: 1600,
+                  child: CustomElevatedButton(
+                    buttonText: "Back Home",
+                    onPressed: () {
+                      Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                          builder: (context) => const LandingPage(),
+                        ),
+                        (route) => true,
+                      );
+                    },
+                    width: 0.9 * screenWidth,
+                    color: primaryColor,
+                  ),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+              ],
+            ),
           ),
         ),
       ),
